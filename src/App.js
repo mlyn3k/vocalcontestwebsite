c48fb63067f22d4ef0df21d19714733f8307d2a9
Lucas <lukaszkarolmlynarczyk@gmail.com>
2023-05-23 14:15:12 +0200

    last simple

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.png';
 import './App.css';
 import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+function Invitation() {
+  return (
+      <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Jak za dawnych lat</h1>
+      <h2>I Przegląd Polskiej Piosenki</h2>
+      <br></br>
+      <p>
+        I Przegląd Polskiej Piosenki to konkurs wokalny, dla solistów - uczestników Środowiskowych Domów Samopomocy z całej Polski.
+      </p>
+      <p>Jesteś zainteresowany?</p> 
+      <br></br>      
+      <Button href='https://forms.gle/jnBHqZi1ob7xmqZn9' variant="contained">Zapisz się!</Button>
+      <br></br><br></br>
+      <p>Zgłoszenia przyjmujemy do 11 czerwca 2023!</p>
+      </>
+  );
+}
+
 
 function App() {
   return (
     <div className='app'>
     <div className="context">
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Jak za dawnych lat</h1>
-        <h2>I Przegląd Polskiej Piosenki</h2>
-        <br></br>
-        <p>
-          I Przegląd Polskiej Piosenki to konkurs wokalny, dla solistów - uczestników Środowiskowych Domów Samopomocy z całej Polski.
-        </p>
-        <p>Jesteś zainteresowany?</p>
-        <br></br>       
-        <Button href='https://forms.gle/jnBHqZi1ob7xmqZn9' variant="contained">Zapisz się!</Button>
+        <Invitation/>
         </div>
   
 
@@ -38,12 +49,8 @@ function App() {
 
         </div>
         <nav className='nav'>
-        <a href='./regulaminPPP.pdf'>
-        <Button variant="outlined">Regulamin</Button>
-        </a>
-        <a href='https://www.facebook.com/konkurspiosenkipolskiej/'>
-        <Button variant="outlined">Facebook</Button>
-        </a>
+        <Button href='https://drive.google.com/file/d/1kAeOEVDHBOCH0yAkZ7CZ1TNoBdVijXsv/view?usp=sharing' variant="outlined">Regulamin</Button> 
+        <Button href='https://www.facebook.com/konkurspiosenkipolskiej/' variant="contained">Facebook</Button>
         </nav>
     <footer>
     <p>Copyright @ 2023</p>
